08b22b3101a1fcf8a5ba1676ce56e15fd067c289
MathieuRalamboson <mathieuralamboson@gmail.com>
2021-09-16 16:22:04 +0200

    Ajout du module Router

--- front/src/app/app-routing.module.ts
+++ front/src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CandidatComponent } from './candidat/candidat.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'candidat', component: CandidatComponent },
+  {
+      path: '**',
+      redirectTo: 'candidat',     
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
